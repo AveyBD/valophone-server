5c184d2f898386fded8b704479bcc984a9ffbcd9
Debashish D. Dev <hello@debashish.dev>
2022-05-23 14:16:24 +0600

    Getting Products From MongoDB

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 const express = require("express");
-const cors = require('cors');
-require('dotenv').config()
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middlware
+app.use(cors());
+app.use(express.json());
 
-// middlware 
-app.use(cors())
-app.use(express.json())
+// mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@valophone.otugk.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const productsCollection = client.db("ValoPhone").collection("products");
+
+    // basic all product api
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productsCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Server is UP!");
