c2c7e30534e95032a050c81521d0d967be176cbf
Debashish D. Dev <hello@debashish.dev>
2022-05-24 14:12:18 +0600

    Sending JSON Web Token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const app = express();
@@ -46,7 +47,12 @@ async function run() {
         updatedDoc,
         options
       );
-      res.send(result);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1h" }
+      );
+      res.send({ result, token });
     });
   } finally {
   }
