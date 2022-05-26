6f6688776ab9ef689fd9bfd73f50702612cb9bec
Debashish D. Dev <hello@debashish.dev>
2022-05-26 12:47:09 +0600

    useAdmin API

--- index.js
+++ index.js
@@ -144,6 +144,14 @@ async function run() {
       res.send(order);
     });
 
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      console.log(email);
+      const user = await usersCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
+    });
+
     // insert user into database
     app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
